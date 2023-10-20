350915cbacf7c739047e5839409fbf7418210989
kaistrecker <k@streckers.de>
2023-10-20 08:57:44 +0200

    no message

--- resources/views/detect.blade.php
+++ resources/views/detect.blade.php
@@ -8,6 +8,12 @@
 
 <body>
 
+GET:
+<?php
+foreach ($_GET as $key => $value) {
+    echo "Parameter: " . $key . ", Wert: " . $value . "<br>";
+}
+?>
 
     <script src="{{ URL::asset('mobile-ios/assets/libs/jquery/jquery-3.6.3.min.js') }}"></script>
 
@@ -24,19 +30,19 @@
             // Prüfen, ob der Benutzer ein iOS-Gerät verwendet
             if (userAgent == "webviewgold") {
                 // Der Benutzer verwendet die App
-                window.location.href = "https://www.alltago.de/splash";
+              //  window.location.href = "https://www.alltago.de/splash";
             } else if (/Android/.test(userAgent)) {
                 // Der Benutzer verwendet ein Android-Gerät
-                window.location.href = "https://www.alltago.app";
+              //  window.location.href = "https://www.alltago.app";
             }  else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
                 // Der Benutzer verwendet ein iOS-Gerät
-                window.location.href = "https://www.alltago.app";
+              //  window.location.href = "https://www.alltago.app";
             } else {
                 // Der Benutzer verwendet weder iOS noch Android
-                window.location.href = "https://www.alltago.app";
+            //    window.location.href = "https://www.alltago.app";
             }
 
-            window.location.href = "https://www.alltago.de/splash";
+          //  window.location.href = "https://www.alltago.de/splash";
         })
 
 
